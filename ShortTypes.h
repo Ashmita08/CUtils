18d03df7f4ac8ddc759661afcb3ff3288b1afc99
Konstantin Tomashevich <ekit.mail@gmail.com>
2018-12-27 21:58:20 +0300

    Initial commit.

---
+++ ShortTypes.h
@@ -0,0 +1,15 @@
+#ifndef __CUTILS_SHORT_TYPES_H__
+#define __CUTILS_SHORT_TYPES_H__
+
+typedef long int lint;
+typedef long long int llint;
+typedef long double ldouble;
+typedef long long double lldouble;
+
+typedef unsigned int uint;
+typedef unsigned short ushort;
+typedef unsigned char uchar;
+typedef unsigned lint ulint;
+typedef unsigned llint ullint;
+
+#endif
