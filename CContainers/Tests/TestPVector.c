c4e1891a0a2275716a3702172fd4caf23dc48812
Konstantin Tomashevich <ekit.mail@gmail.com>
2019-01-09 19:39:39 +0300

    TestPVector: fix typo.

--- CContainers/Tests/TestPVector.c
+++ CContainers/Tests/TestPVector.c
@@ -15,7 +15,7 @@ void PVectorSuite_Helper_CheckSize (uint size)
     if (size != PVector_Size (pVectorHandle))
     {
         printf ("\n        Size (e/a): %d, %d.", size, PVector_Size (pVectorHandle));
-        CU_FAIL_FATAL ("Expected and equal values are not equal!");
+        CU_FAIL_FATAL ("Expected and actual values are not equal!");
     }
 }
 
